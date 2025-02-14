9f76befc90b5a160e6b1417d28c14db96392f532
karen chahrour <karenchahrour123@gmail.com>
2025-02-14 13:59:32 +0100

    Creating-greetings.py

---
+++ greetings.py
@@ -0,0 +1,13 @@
+
+
+number = 100
+print(number)
+
+age = 12
+print("you are " , age , "years old")
+name = input("please enter your name: ")
+year = input("Please enter your year of birth: ")
+
+age = 2025 - int(year)
+print(name, "is", age , " years old")
+

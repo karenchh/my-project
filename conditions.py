03017bf9255cf0d6cc2138fe05081cb5612290a5
karen chahrour <karenchahrour123@gmail.com>
2025-02-14 14:02:45 +0100

    creating-conditionfile

---
+++ conditions.py
@@ -0,0 +1,12 @@
+age = 12
+#age = int(input("Please enter your age: ")) or age = input("....") then number = int(age)
+
+#if age = 20 (try)
+
+if age > 15 : 
+    print("You can run this code ")
+else:
+    print ("Unauthorized")
+    
+print("Program is done ") #OUTSIDE the if statment in both ways it will be written 
+
